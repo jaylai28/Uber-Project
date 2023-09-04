24489d50675b6fdc198ebfb97ed5a998f5b03ff5
Jay Lai <weijianlai28@gmail.com>
2023-09-04 18:17:37 +1000

    Updated Mage Files and Command files

---
+++ Mage Files/uber_exporter.py
@@ -0,0 +1,34 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.snowflake import Snowflake
+from pandas import DataFrame
+from os import path
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_snowflake(data: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to a Snowflake warehouse.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#snowflake
+    """
+    database = 'UBER_DATA_ENGINEERING'
+    schema = 'STAGING'
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    for key, value in data.items():
+        table_name = key
+
+        with Snowflake.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
+            loader.export(
+                DataFrame(value),
+                table_name,
+                database,
+                schema,
+                if_exists='replace',  # Specify resolution policy if table already exists
+            )
